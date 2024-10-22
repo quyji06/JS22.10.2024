bb0b1d916d2a2aa4572a6d62d02420c9d0b71fde
Kamil6747 <kamlbaron3523@gmail.com>
2024-10-22 08:25:06 +0200

    js1

---
+++ script.js
@@ -0,0 +1,45 @@
+/*Zadanie 1
+Stwórz zmienną imie i przypisz jej swoje imię. Następnie użyj console.log(), aby wyświetlić tę wartość w konsoli.*/
+
+let imie = "Ania";
+console.log(imie);
+
+
+/*Zadanie 2
+Stwórz zmienną wiek i przypisz jej wartość liczbową swojego wieku. Zaktualizuj tę zmienną o 1, aby
+symulować upływający rok, i wyświetl nową wartość w konsoli*/
+
+
+let wiek = 15;
+let rok = 1;
+console.log(wiek + rok); 
+
+/*Zadanie 3
+Stwórz dwie zmienne a i b z wartościami liczbowymi, a następnie oblicz ich sumę, różnicę, iloczyn i
+iloraz. Wyniki wyświetl w konsoli.*/
+
+let a = 2;
+let b = 4;
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+
+/*Zadanie 4
+Stwórz zmienną czyPrawda i przypisz jej wartość true. Następnie użyj operatora negacji (!), aby zmienić
+jej wartość na przeciwną i wyświetl wynik w konsoli.*/
+
+let czyPrawda = true;
+console.log(!czyPrawda);
+
+/*Zadanie 5
+Stwórz zmienną tekst i przypisz jej dowolny tekst. Użyj operatora typeof, aby sprawdzić i wyświetlić typ
+tej zmiennej.*/
+
+let text = "kod"
+console.log(typeof text);
+
+
+/*Zadanie 6
+Stwórz zmienną x o wartości 10 i y o wartości "10". Użyj operatorów == oraz ===, aby sprawdzić, czy
+wartości te są sobie równe. Wyjaśnij wynik w komentarzu*/
